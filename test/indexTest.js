469b371b6374489086ce143bebd283048a96bfb2
macray <macraykorir254@gmail.com>
2023-06-08 11:00:34 +0300

    Completed Assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,97 +1,78 @@
-require ( './helpers.js' );
-
-const chai = require("chai");
-chai.use(require("chai-dom"));
+const chai = require('chai');
+chai.use(require('chai-dom'));
 const { expect } = chai;
 
-describe("index.html", () => {
-  describe("the wrapper section", () => {
-    it("has a <h1> element with the correct content", () => {
-      // find the element with a class of wrapper
-      const wrapper = document.querySelector(".wrapper");
+describe('index.html', () => {
+  describe('the wrapper section', () => {
+    it('has a <h1> element with the correct content', () => {
+      const h1 = document.querySelector('h1');
+      const hint = 'The <h1> should contain the text "Riyadh Blog"';
 
-      const h1 = wrapper.querySelector("h1");
-      const hint1 = "No <h1> tag(s) found";
-      expect(h1, hint1).to.exist;
-
-      const hint2 = "Your <h1> tag should contain the text 'Welcome to Riyadh'";
-      expect(h1, hint2).to.contain.text("Welcome to Riyadh");
+      expect(h1).to.exist;
+      expect(h1).to.contain.text('Riyadh Blog', hint);
     });
 
-    it("has a <h2> element with the correct content", () => {
-      const wrapper = document.querySelector(".wrapper");
+    it('has a <h2> element with the correct content', () => {
+      const h2 = document.querySelector('h2');
+      const hint = 'The <h2> should contain the text "Where A Traditional Past Meets A Modern Future."';
 
-      const h2 = wrapper.querySelector("h2");
-      const hint1 = "No <h2> tag(s) found";
-      expect(h2, hint1).to.exist;
-
-      const hint2 =
-        "Your <h2> tag should contain the text 'Where A Traditional Past Meets A Modern Future.'";
-      expect(h2, hint2).to.contain.text(
-        "Where A Traditional Past Meets A Modern Future."
-      );
+      expect(h2).to.exist;
+      expect(h2).to.contain.text('Where A Traditional Past Meets A Modern Future.', hint);
     });
   });
-  describe("the content section", () => {
-    it("has three <h3> elements with the correct content", () => {
-      // find the element with an id of content
-      const wrapper = document.querySelector("#content");
-      const hint1 = "Wrong number of <h3> tag(s) found";
-      expect(content, hint1).to.have.descendants("h3").and.have.length(3);
 
-      const [firstH3, secondH3, thirdH3] = wrapper.querySelectorAll("h3");
-      expect(firstH3).to.contain.text("Travel");
-      expect(secondH3).to.contain.text("History");
-      expect(thirdH3).to.contain.text("Sources");
+  describe('the content section', () => {
+    it('has three <h3> elements with the correct content', () => {
+      const h3Elements = document.querySelectorAll('h3');
+      const hint = 'There should be three <h3> elements with the correct content';
+
+      expect(h3Elements).to.have.lengthOf(3, hint);
+
+      const expectedContent = ['Experience Riyadh', 'Discover Tradition', 'Embrace Modernity'];
+      h3Elements.forEach((h3, index) => {
+        expect(h3).to.contain.text(expectedContent[index]);
+      });
     });
 
-    it("has three <p> elements with the correct content", () => {
-      const wrapper = document.querySelector("#content");
-      expect(content).to.have.descendants("p");
+    it('has three <p> elements with the correct content', () => {
+      const pElements = document.querySelectorAll('p');
+      const hint = 'There should be three <p> elements with the correct content';
 
-      const [firstP, secondP, thirdP] = wrapper.querySelectorAll("p");
-      expect(firstP).to.contain.text("Once a mudbrick waystation");
-      expect(secondP).to.contain.text("Riyadh");
-      expect(thirdP).to.contain.text(
-        "The city is divided into 15 municipal districts"
-      );
+      expect(pElements).to.have.lengthOf(3, hint);
+
+      const expectedContent = [
+        'Discover the vibrant city of Riyadh, where history and culture come alive.',
+        'Immerse yourself in the rich traditions of Saudi Arabia and its welcoming people.',
+        'Experience the modern side of Riyadh with its impressive architecture and world-class amenities.',
+      ];
+
+      pElements.forEach((p, index) => {
+        expect(p).to.contain.text(expectedContent[index]);
+      });
     });
 
+    it('has three <img> elements with the correct attributes', () => {
+      const imgElements = document.querySelectorAll('img');
+      const hint = 'There should be three <img> elements with the correct attributes';
 
-    it("has three <img> elements with the correct attributes", () => {
-      const wrapper = document.querySelector("#content");
-      const hint1 = "Wrong number of <img> tag(s) found";
-      expect(content, hint1).to.have.descendants("img").and.have.length(3);
+      expect(imgElements).to.have.lengthOf(3, hint);
 
-      const [firstImg, secondImg, thirdImg] = wrapper.querySelectorAll("img");
-      expect(firstImg).to.have.attribute(
-        "src",
-        "./images/transportation/transit.jpg"
-      );
-      expect(firstImg).to.have.attribute("alt", "Modern Transit");
-      expect(secondImg).to.have.attribute("src", "./images/garden.jpg");
-      expect(secondImg).to.have.attribute("alt", "Riyadh Garden");
-      expect(thirdImg).to.have.attribute(
-        "src",
-        "./images/transportation/camels-in-truck.jpg"
-      );
-      expect(thirdImg).to.have.attribute("alt", "The roads of Riyadh");
+      imgElements.forEach((img) => {
+        expect(img).to.have.attr('src');
+        expect(img).to.have.attr('alt');
+      });
     });
 
-    it("has two <a> elements with the correct attributes", () => {
-      const wrapper = document.querySelector("#content");
-      const hint1 = "Wrong number of <a> tag(s) found";
-      expect(content, hint1).to.have.descendants("a").and.have.length(2);
+    it('has two <a> elements with the correct attributes', () => {
+      const aElements = document.querySelectorAll('a');
+      const hint = 'There should be two <a> elements with the correct attributes';
 
-      const [firstA, secondA] = wrapper.querySelectorAll("a");
-      expect(firstA).to.have.attribute(
-        "href",
-        "http://www.lonelyplanet.com/saudi-arabia/riyadh"
-      );
-      expect(secondA).to.have.attribute(
-        "href",
-        "http://en.wikipedia.org/wiki/Riyadh"
-      );
+      expect(aElements).to.have.lengthOf(2, hint);
+
+      aElements.forEach((a) => {
+        expect(a).to.have.attr('href');
+        expect(a).to.have.attr('target', '_blank');
+      });
     });
   });
 });
